4979f3db895a83c4644fed6e8d55ea85660b8361
Phoenix661T <thabangmaloka661@outlook.com>
2022-04-18 14:33:27 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,44 +1,27 @@
 #!/usr/bin/python3
-'''
-Starts the Flask web application
-'''
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
-from flasgger import Swagger, swag_from
+"""app.py to connect to API"""
+import os
 from models import storage
 from api.v1.views import app_views
-from os import environ
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, origins="0.0.0.0")
-app.config['SWAGGER'] = {
-        'title': 'HBNB API',
-        'description': 'RESTFul API for HBNB'
-    }
-swag = Swagger(app)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_storage(exception):
-    """Closes storage session"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
+def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-
-if __name__ == '__main__':
-    """Runs the app"""
-    if environ.get('HBNB_API_HOST'):
-        host = environ.get('HBNB_API_HOST')
-    else:
-        host = '0.0.0.0'
-
-        if environ.get('HBNB_API_PORT'):
-            port = environ.get('HBNB_API_PORT')
-        else:
-            port = 5000
-
-        app.run(host=host, port=port, threaded=True)
+if __name__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
